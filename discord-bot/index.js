04bb9db008bb2e487771135d74d584301ac4fdba
tclaudel <zekidok@gmail.com>
2020-05-14 13:49:36 +0200

    user object created

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const botId = '709771830222651492';
-var users;
+var users = new Array();
 
 function User(id, username) {
   this.id = id;
@@ -15,15 +15,18 @@ client.on('ready', () => {
 client.on('message', message => {
 if (message.author.id != botId) {
 	if (message.content === '!subscribe') {
-    // message.channel.send(message.author.username + ' subcribed to the bootcamp');
-    senderId = message.author.id
-    senderUsername = message.author.username
     let myRole = message.guild.roles.cache.get("710414732594511894");
     message.member.roles.add(myRole).catch(console.error);
     var user = new User(message.author.id, message.author.username);
-    message.channel.send("Sender id : " + user.id);
-    message.channel.send("Sender name : " + user.username);
+    console.log("Sender id : " + user.id);
+    console.log("Sender name : " + user.username);
+    users.push(user);
+  }
+  else if (message.content === '!list') {
+    for (let i = 0; i < users.length; i++)
+      console.log("User " + i + " name : " + users[i].username)
+    }
   }
 });
 
-client.login('NzA5NzcxODMwMjIyNjUxNDky.Xr0mbg.SjvKUCv5SJKSX048mpgg7iZvrG4');
+client.login('token');
