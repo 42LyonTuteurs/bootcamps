f26a1dcaad56bfa00443591655c00aaad3f27427
tclaudel <zekidok@gmail.com>
2020-05-19 16:27:43 +0200

    help done

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -10,7 +10,6 @@ const config = configFile.botConfig;
 const client = new Discord.Client();
 var faker = require('faker');
 var users = new Array();
-const antispam = require('better-discord-antispam'); // Requiring this module.
 const PREFIX = '!';
 
 
@@ -45,37 +44,48 @@ function User(id, username) {
 
 async function subscribe(message)
 {
-  let myRole = message.guild.roles.cache.get(config.bootcampRoleId);
-  message.member.roles.add(myRole).catch(console.error);
-  var user = new User(message.member.id, message.member.nickname);
-  utils.logs("subscribtion of :" + user.username + " " + user.id);
-  users.push(user);
-  await utils.addUser(user.id, user.username);
-  if (!message.guild.channels.cache.map(t => t.name).includes("bootcamp-" + message.member.nickname)) {
-    const everyoneRole = message.guild.roles.cache.get(config.everyoneRoleId);
-    const PrivateChannelWithBot = "bootcamp " + message.member.nickname;
-    message.guild.channels.create(PrivateChannelWithBot, {
-      type: "text",
-      parent: config.privateChannelCategoryId,
-      permissionOverwrites: [
-        {
-          id: everyoneRole,
-          deny: ['VIEW_CHANNEL'],
-        },
-        {
-          id: message.author.id,
-          allow: ['VIEW_CHANNEL'],
-        },
-      ],})
+  try {
+    let myRole = message.guild.roles.cache.get(config.bootcampRoleId);
+    message.member.roles.add(myRole).catch(console.error);
+    var user = new User(message.member.id, message.member.nickname);
+    utils.logs("subscribtion of :" + user.username + " " + user.id);
+    users.push(user);
+    await utils.addUser(user.id, user.username);
+    // if (!message.guild.channels.cache.map(t => t.name).includes("bootcamp-" + message.member.nickname)) {
+      const everyoneRole = message.guild.roles.cache.get(config.everyoneRoleId);
+      const PrivateChannelWithBot = "bootcamp " + message.member.nickname;
+      message.guild.channels.create(PrivateChannelWithBot, {
+        type: "text",
+        parent: config.privateChannelCategoryId,
+        permissionOverwrites: [
+          {
+            id: everyoneRole,
+            deny: ['VIEW_CHANNEL'],
+          },
+          {
+            id: message.author.id,
+            allow: ['VIEW_CHANNEL'],
+          },
+        ],})
         .then(r => {
-          r.send("Here is your private channel with the bot, please enter here your commands to interract with the bot");
+          r.send("@" + message.member.nickname + "\n```Here is your private channel with the bot, please enter here your commands to interract with the bot```");
         })
         .catch(console.error);
+    // }
+    message.channel.send("```" + message.member.nickname + " has been successfully subscribed !\nA Channel has just been created for you in Bootcamp " + 
+    "category\nPlease write your commands there to interact with the bot\nThis message will self-destroyed in 10s```")
+      .then(msg => {
+        msg.delete({ timeout: 15000 })
+      });
+  } catch {
+    this.logs("ERROR : subscription failed : " + e);
+    message.channel.send("ERROR : subscription failed : " + e);
   }
+
 };
 
 async function unsubscribe(message)
-{
+{0
   await utils.deleteUserByLogin(message.member.nickname);
   let channelToDestroy;
   let index = users.findIndex(u => u.username == message.member.nickname);
@@ -117,7 +127,14 @@ async function info(message, argv)
 
 function help(message)
 {
-  message.channel.send("!help");
+  let str = "__**HELP MENU**__\n\n"+
+  "You will find all the commands you can use in this discord just behind :\n\n" +
+  "**!subscribe**\n> to subscribe to the bootcamp, a private channel will be created\n\n" +
+  "**!info**\n> to diplay info from yourself or from other participant with *!info <login>*\n\n" +
+  "**!unsubscribe**\n> to unsubscribe from the bootcamp\n\n" +
+  "**!validated <login> <day> <validated>**\n> to tell the bot that you corrected the <day> of <login> and if the day is <validated> or <notvalidated>\n\n" +
+  "**!corrected by <login> <day>**\n> to tell the bot that your <day> have been corrected by <login>\n\n";
+  message.channel.send(str);
 }
 
 async function fakerDb()
@@ -128,25 +145,9 @@ async function fakerDb()
     await utils.addUser(user.id, user.username);
     users.push(user);
   }
-  // users.push (await utils.addUser("220625639655473152", "tclaudel"));
-  // users.push (await utils.addUser("374265216608763907", "jdarko"));
 }
 
 client.on('ready', async() => {
-  antispam(client, {
-    limitUntilWarn: 3, // The amount of messages allowed to send within the interval(time) before getting a warn.
-    limitUntilMuted: 5, // The amount of messages allowed to send within the interval(time) before getting a muted.
-    interval: 20000, // The interval(time) where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted. (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc etc)
-    warningMessage: "if you don't stop from spamming, I'm going to punish you!", // Message you get when you are warned!
-    muteMessage: "was muted since we don't like too much advertisement type people!", // Message sent after member X was punished(muted).
-    maxDuplicatesWarning: 7,// When people are spamming the same message, this will trigger when member X sent over 7+ messages.
-    maxDuplicatesMute: 10, // The limit where member X get muted after sending too many messages(10+).
-    // ignoredRoles: ["Admin"], // The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys. Also it's case sensitive.
-    // ignoredMembers: ["Mavis#2389"], // These members are directly affected and they do not require to have the role above. Good for undercover pranks.
-    mutedRole: "muted", // Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
-    timeMuted: 1000 * 600, // This is how much time member X will be muted. if not set, default would be 10 min.
-    logChannel: "antispam-logs" // This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
-  });
   if (await utils.UserNb() < 20)
     await fakerDb();
   fs.writeFile('app.log', "", (err) => {
@@ -158,8 +159,6 @@ client.on('ready', async() => {
 });
 
 client.on('message', async message => {
-  // console.log(message.guild.roles.cache);
-  utils.logs("send : "+ message.content, message.member);
   LoginList = await utils.AllLogin();
   if (message.author.username != "bootcamp" && !message.author.bot && message.content.search("!mana") == -1)
   {
@@ -206,8 +205,15 @@ client.on('message', async message => {
         c.corrected(message, commandArgs.split(" "))
       else if (command === 'validated')
         c.validated(message, commandArgs.split(" "))
-      else
+      else if (command === 'help')
         help(message);
+      else {
+        message.delete();
+        message.channel.send("```" + message.content + " is an unknown function, please try !help```")
+        .then(msg => {
+          msg.delete({ timeout: 10000 })
+        });
+      }
     }
   }
 });
